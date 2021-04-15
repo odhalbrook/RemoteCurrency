40d1caa0675fd1d552e6d1bda51b4a17f62e3c2f
odhalbrook <33768369+odhalbrook@users.noreply.github.com>
2021-04-14 21:13:18 -0600

    Finished41421

--- CurrencyPOS/MainWorker.java
+++ CurrencyPOS/MainWorker.java
@@ -6,27 +6,42 @@ public class MainWorker {
 	static Scanner uI = new Scanner (System.in);
 	static int [] bases = new int [8];
 	static int [] currents = new int [8];
+	static int refinei = 0;
 	
 	public static void main(String[]args)
 	{
 		readFromFile();
+		System.out.println("How much does the item cost?");
+		int co = uI.nextInt();
+		System.out.println("How much are you giving the cashier?");
+		int ra = uI.nextInt();
+		refinei = ra - co;
 		work(0);
 	}
-	private static void work(int num) {
-		
-		if(num ==9)
+	private static void work(int num) 
+	{
+		if(num ==8)
 		{
 			result();
 		}
 		else
 		{
-		int cur = currents[num];
-		
-		work(num++);
+		int ba = bases[num];
+		if(ba<=refinei)
+		{
+			currents[num] = refinei/ba;
+			
+			refinei = refinei % ba;
+		}
+		num++;
+		work(num);
 		}
 	}
 	private static void result() {
-		// TODO Auto-generated method stub
+		for (int d = 0; d<8; d++)
+		{
+			System.out.println(bases[d] + " = " + currents[d]);
+		}
 		System.exit(0);
 	}
 	public static void readFromFile()
